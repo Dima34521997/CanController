50b0f2efff4d5251b05906e24d531de5330b8d59
dima1997mail@gmail.com <dima1997mail@gmail.com>
2023-09-18 17:21:03 +0700

    Добавлена функции записи массива в OD

--- _CAN Test/SDOcommunication.cs
+++ _CAN Test/SDOcommunication.cs
@@ -34,12 +34,12 @@ public static class SDOcommunication
     }
 
 
-    public static T[] ReadArraySDO<T>(byte node, UInt16 Index, byte SubIndex, T[] data) // чтение
+    public static T[] ReadArraySDO<T>(byte node, UInt16 Index, T[] data) 
     {
         UInt32 DataSize = (UInt16)TypeSize<T>.Size;
         Int16 functionResultCode = 0;
-        ushort ArraySize = GetLengthOfArray(node, Index);
-        
+        UInt16 ArraySize = GetLengthOfArray(node, Index);
+
         for (byte index = 0; index < ArraySize; index++)
         {
             data[index] = ReadSDO(node, Index, (byte)(0x01 + index), data[index]);
@@ -54,37 +54,37 @@ public static class SDOcommunication
     #endregion
 
     #region Запись
-    public static T WriteSDO<T>(byte node, UInt16 Index, byte SubIndex, T data) // чтение
+    public static void WriteSDO<T>(byte node, UInt16 Index, byte SubIndex, T data) 
     {
+        T newData = data;
         UInt32 DataSize = (UInt16)TypeSize<T>.Size;
         Int16 functionResultCode = 0;
 
         unsafe
         {
-            functionResultCode =
-                CANOpenDll.read_device_object_sdo(node, Index, SubIndex, (byte*)&data, ref DataSize);
+            functionResultCode = CANOpenDll.
+                write_device_object_sdo(node, Index, SubIndex, (byte*)&newData, DataSize);
+            
         }
 
-        if (functionResultCode != 0 && data == null) // типа пустой индекс
-            throw new Exception("Попытка прочесть пустой индекс");
-
-        return data;
+        if (functionResultCode != 0) throw new Exception("Ошибка записи");
     }
 
 
 
-    static public void WriteSDO(byte node, UInt16 Index, byte SubIndex, in byte Data)// 1) byte
+    static public void WriteArraySDO<T>(byte node, UInt16 Index, T[] data) 
     {
-        byte newData = Data;
-        UInt32 DataSize = sizeof(byte);
+        T[] newData = data;
+        UInt16 ArraySize = GetLengthOfArray(node, Index);
 
-        Int16 fnr = 0;
 
-        unsafe
+        for (byte subIndex = 1; subIndex < ArraySize; subIndex++)
         {
-            fnr = CANOpenDll.write_device_object_sdo(node, Index, SubIndex, (byte*)(&newData), DataSize);
+            WriteSDO(node, Index, (byte)(subIndex), data[subIndex - 1]);
+            Thread.Sleep(1);
         }
     }
+
 
 
     #endregion
@@ -110,7 +110,7 @@ public static class SDOcommunication
     //    }
     //}
 
-     private static UInt16 GetLengthOfArray(byte node, UInt16 Index)
+    private static UInt16 GetLengthOfArray(byte node, UInt16 Index)
     {
         UInt16 Size = sizeof(byte);
         Size = ReadSDO(node, Index, 0x00, Size);
